c8ec983eea2be58a0615d03e87c0c6f24ed88dfc
Ray Liao <43103986+rayzliao8787@users.noreply.github.com>
2018-09-30 10:18:10 +0800

    Update Date.py

--- Date.py
+++ Date.py
@@ -3,7 +3,11 @@ from datetime import date
 def differ_days(date1, date2):
     a = date1
     b = date2
-    return (a-b).days
+    return ((a-b).days) + 1
 dt1 = input("First year: ")
+mt1= input("First month: ")
+dy1 = input("First day: ")
 dt2 = input("Second year: ")
-print(differ_days((date(int(dt1),1,1)), date(int(dt2),1,1)))
+mt2 = input("Second month: ")
+dy2 = input("Second day: ")
+print(differ_days((date(int(dt1),int(mt1),int(dy1))), date(int(dt2),int(mt2),int(dy2))))
